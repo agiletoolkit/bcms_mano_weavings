b7db16b9f5689122151798a63a8bdae8273130c4
Raymond Kortas <rjck2@students.waikato.ac.nz>
2009-12-06 21:18:29 +1300

    Added seed data to db/seeds.rb which creates a Weavings section and a few pages to put portlets. Updated the installation instructions to indicate that rake db:seed can optionally be run to generate the basic structure.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+class LoadWeavingsSeedData < ActiveRecord::Migration
+  extend Cms::DataLoader
+    create_section(:weavings, :name => "Weavings", :parent => Section.find_by_path("/"), :path => "/weavings")
+    Group.all.each{|g| g.sections = Section.all }
+
+    create_page(:overview, :name => "Overview", :path => "/weavings", :section => sections(:weavings), :template_file_name => "default.html.erb", :publish_on_save => true, :hidden => false, :cacheable => true)
+    create_page(:weaving_process, :name => "Weaving Process", :path => "/weavings/weaving-process", :section => sections(:weavings), :template_file_name => "default.html.erb", :publish_on_save => true, :hidden => false, :cacheable => true)
+    create_page(:weavers, :name => "Weavers", :path => "/weavings/weavers", :section => sections(:weavings), :template_file_name => "default.html.erb", :publish_on_save => true, :hidden => false, :cacheable => true)
+    create_page(:weavings, :name => "Weavings", :path => "/weavings/weavings", :section => sections(:weavings), :template_file_name => "default.html.erb", :publish_on_save => true, :hidden => false, :cacheable => true)
+    create_page(:items_for_sale, :name => "Items for Sale", :path => "/weavings/items-for-sale", :section => sections(:weavings), :template_file_name => "default.html.erb", :publish_on_save => true, :hidden => false, :cacheable => true)
+    create_page(:donate, :name => "Donate", :path => "/weavings/donate", :section => sections(:weavings), :template_file_name => "default.html.erb", :publish_on_save => true, :hidden => false, :cacheable => true)
+end
+
+# Create some initial pages the Weavings Module will need for portlets etc.
+# Should put here because it is seed data and should be run after all the migrations.
+LoadWeavingsSeedData.new
