210318e80351a47f2c6d4b5a3758d6e7c526f120
Raymond Kortas <rjck2@students.waikato.ac.nz>
2009-12-24 22:11:27 +1300

    Added google shopping cart integration.

--- app/models/weaving.rb
+++ app/models/weaving.rb
@@ -4,4 +4,7 @@ class Weaving < ActiveRecord::Base
   belongs_to :weaving_type
   belongs_to :wool_type
 
+  def add_to_google_cart_button_html
+    '<div class="product"><input type="hidden" class="product-title" value="Weaving"><input type="hidden" class="product-price" value="' + selling_price.to_s + '"><div class="googlecart-add-button" tabindex="0" role="button" title="Add to cart"></div></div>'
+  end
 end
