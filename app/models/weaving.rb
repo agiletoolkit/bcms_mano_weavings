9b207ce850ed3226ebebdf3fbcbd242f173769f4
Raymond Kortas <rjck2@students.waikato.ac.nz>
2010-01-11 16:48:07 +1300

    Removed the google checkout cart code because it does not provide all the required features.

--- app/models/weaving.rb
+++ app/models/weaving.rb
@@ -4,8 +4,4 @@ class Weaving < ActiveRecord::Base
   belongs_to :weaving_type
   belongs_to :wool_type
   belongs_to :cart
-
-  def add_to_google_cart_button_html
-    '<div class="product"><input type="hidden" class="product-title" value="Weaving"><input type="hidden" class="product-price" value="' + selling_price.to_s + '"><div class="googlecart-add-button" tabindex="0" role="button" title="Add to cart"></div></div>'
-  end
 end
